89141d24f32401872e52dec1d16349736101de3c
Scott Shawcroft <scott@tannewt.org>
2024-05-17 21:31:04 -0700

    Add link function

--- embedded/compiler.py
+++ embedded/compiler.py
@@ -31,3 +31,16 @@ class Clang(Compiler):
         output_file.parent.mkdir(parents=True, exist_ok=True)
         cpu_flags = cpu.get_arch_cflags(self)
         await build.run_command([self.c_compiler, *cpu_flags, "-MMD", "-c", source_file, *flags, "-o", output_file], description=f"Compile {source_file.relative_to(cwd)} -> {output_file.relative_to(cwd)}", working_directory=caller_directory)
+    
+    @build.capture_caller_directory
+    async def link(self, cpu, objects: list[pathlib.Path], output_file: pathlib.Path, linker_script: pathlib.Path, flags: list[str] = [], print_memory_use=True, output_map_file=True, gc_sections=True, caller_directory=None):
+        output_file.parent.mkdir(parents=True, exist_ok=True)
+        cpu_flags = cpu.get_arch_cflags(self)
+        link_flags = []
+        if print_memory_use:
+            link_flags.append("-Wl,--print-memory-usage")
+        if output_map_file:
+            link_flags.append("-Wl,-Map=" + str(output_file.with_suffix(".elf.map").relative_to(caller_directory)))
+        if gc_sections:
+            link_flags.append("-Wl,--gc-sections")
+        await build.run_command([self.c_compiler, *cpu_flags, *link_flags, *flags, *objects, "-T", linker_script, "-o", output_file], description=f"Link {output_file.relative_to(cwd)}", working_directory=caller_directory)
